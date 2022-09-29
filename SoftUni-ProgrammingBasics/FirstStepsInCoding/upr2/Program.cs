49ce664cc05550e369bbe2c0358bc781b6eae2ae
User <georgi010205@gmail.com>
2022-09-29 22:41:16 +0300

    Solved a problem

---
+++ SoftUni-ProgrammingBasics/FirstStepsInCoding/upr2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace upr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int srok = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double lihva = deposit * (procent / 100);
+            double lihva1mesec = lihva / 12;
+            Console.WriteLine(deposit + srok * lihva1mesec);
+        }
+    }
+}
