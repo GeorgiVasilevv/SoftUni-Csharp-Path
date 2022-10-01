bf52f318a0d75e518a16ec060770f238ffcccde9
User <georgi010205@gmail.com>
2022-10-01 17:58:13 +0300

    Add files via upload

---
+++ SoftUni-ProgrammingBasics/FundamentalsStart/ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double avarageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: { age}, Grade: {avarageGrade:f2}");
+        }
+    }
+}
