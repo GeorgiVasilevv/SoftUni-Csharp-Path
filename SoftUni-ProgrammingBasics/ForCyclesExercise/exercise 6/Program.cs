bf327d7b07d60f20a058535377c2f05bb4ad642e
User <georgi010205@gmail.com>
2022-10-07 19:47:01 +0300

    Add files via upload

---
+++ SoftUni-ProgrammingBasics/ForCyclesExercise/exercise 6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int peopleSum = 0;
+            double musala = 0, monblan = 0, killimandjaro = 0, k2 = 0, everest = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                peopleSum = peopleSum + people;
+                if (people <= 5)
+                {
+                    musala=musala+people;
+                }
+                else if (people <= 12)
+                {
+                    monblan = monblan + people;
+                }
+                else if (people <= 25)
+                {
+                    killimandjaro = killimandjaro + people;
+                }
+                else if (people <= 40)
+                {
+                    k2 = k2 + people;
+                }
+                else if (people >= 41)
+                {
+                    everest = everest + people;
+                }
+            }
+            musala = musala / peopleSum * 100;
+            monblan = monblan / peopleSum * 100;
+            killimandjaro = killimandjaro / peopleSum * 100;
+            k2 = k2 / peopleSum * 100;
+            everest = everest / peopleSum * 100;
+            Console.WriteLine($"{musala:f2}%");
+            Console.WriteLine($"{monblan:f2}%");
+            Console.WriteLine($"{killimandjaro:f2}%");
+            Console.WriteLine($"{k2:f2}%");
+            Console.WriteLine($"{everest:f2}%");
+
+        }
+    }
+}
---
+++ SoftUni-ProgrammingBasics/ForCyclesExercise/exercise7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int points = int.Parse(Console.ReadLine());
+            int pointsCountW = 0, pointsCountF = 0, pointsCountSF = 0;
+            double percentCount = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string result = Console.ReadLine();
+
+                if (result== "W")
+                {
+                    points = points + 2000;
+                    pointsCountW = pointsCountW + 2000;
+                    percentCount++;
+                }
+                else if (result== "F")
+                {
+                    points = points + 1200;
+                    pointsCountF = pointsCountF + 1200;
+                }
+                else if (result== "SF")
+                {
+                    points = points + 720;
+                    pointsCountSF = pointsCountSF + 720;
+                }
+            }
+            double avargePoints= (pointsCountW+ pointsCountF+ pointsCountSF)/ n;
+            avargePoints = Math.Floor(avargePoints);
+            double percent = percentCount / n * 100;
+            Console.WriteLine($"Final points: {points}");
+            Console.WriteLine($"Average points: {avargePoints}");
+            Console.WriteLine($"{percent:f2}%");
+        }
+    }
+}
