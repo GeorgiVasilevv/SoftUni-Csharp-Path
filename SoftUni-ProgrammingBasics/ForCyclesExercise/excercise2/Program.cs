b1fd959ca0820a0582e289e943820a848f15ca35
User <georgi010205@gmail.com>
2022-10-07 19:36:30 +0300

    Add files via upload

---
+++ SoftUni-ProgrammingBasics/ForCyclesExercise/excercise2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace excercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 200)
+                {
+                    p1++;
+                }
+                else if (num >= 200 && num < 400)
+                {
+                    p2++;
+                }
+                else if (num >= 400 && num < 600)
+                {
+                    p3++;
+                }
+                else if (num >= 600 && num < 800)
+                {
+                    p4++;
+                }
+                else if (num >= 800)
+                {
+                    p5++;
+                }
+            }
+            p1 = p1 / n * 100;
+            p2 = p2 / n * 100;
+            p3 = p3 / n * 100;
+            p4 = p4 / n * 100;
+            p5 = p5 / n * 100;
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+            Console.WriteLine($"{p4:f2}%");
+            Console.WriteLine($"{p5:f2}%");
+
+        }
+    }
+}
