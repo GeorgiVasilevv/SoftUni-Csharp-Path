8a9281388c9b51eda1a1a5d181f46655322400c7
User <georgi010205@gmail.com>
2022-10-07 19:31:56 +0300

    Add files via uplaod

---
+++ SoftUni-ProgrammingBasics/MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double land = double.Parse(Console.ReadLine());
+            double final = land * 7.61;
+            double disscount = final * 0.18;
+            double diss = final - disscount;
+            Console.WriteLine($"The final price is: {diss} lv.");
+            Console.WriteLine($"The discount is: {disscount} lv.");
+        }
+    }
+}
