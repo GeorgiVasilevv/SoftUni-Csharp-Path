37b0dc94392f0bbb790814003d2e05f4797d226e
User <georgi010205@gmail.com>
2022-10-09 18:20:17 +0300

    Add files via upload

---
+++ SoftUni-ProgrammingBasics/WhileLoops/exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (num > sum)
+            {
+                int currentnum = int.Parse(Console.ReadLine());
+                sum += currentnum;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
