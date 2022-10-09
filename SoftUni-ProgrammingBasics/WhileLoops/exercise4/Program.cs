5118e8556d202bc28f5d512b9241f181dfd1d138
User <georgi010205@gmail.com>
2022-10-09 18:21:04 +0300

    Add files via upload

---
+++ SoftUni-ProgrammingBasics/WhileLoops/exercise4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int k = 1;
+
+            while (k<=num)
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1;
+            }
+        }
+    }
+}
