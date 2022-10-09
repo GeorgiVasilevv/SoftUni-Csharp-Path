8fb603d0d5423e5e2aefcf4b5d313e14855b75c8
User <georgi010205@gmail.com>
2022-10-09 18:18:08 +0300

    Add files via upload

---
+++ SoftUni-ProgrammingBasics/WhileLoops/7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int minNum = int.MaxValue;
+
+            while (command != "Stop")
+            {
+                int currentNum = int.Parse(command);
+
+                if (currentNum < minNum)
+                {
+                    minNum = currentNum;
+                }
+                command = Console.ReadLine();
+
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
