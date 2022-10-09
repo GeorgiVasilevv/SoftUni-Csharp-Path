e3c887a35ad98bb2b826293104cddadd2134b9f2
User <georgi010205@gmail.com>
2022-10-09 18:23:29 +0300

    Add files via upload

---
+++ SoftUni-ProgrammingBasics/WhileLoops/exercise5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double bankAccount = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double sum = double.Parse(input);
+                if (sum<0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                bankAccount += sum;
+                Console.WriteLine($"Increase: {sum:f2}");
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Total: {bankAccount:f2}");
+        }
+    }
+}
