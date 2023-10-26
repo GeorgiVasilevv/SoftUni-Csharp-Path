e7975e55ebb1ca4d7e59821c1750459f805d9f85
User <georgi010205@gmail.com>
2023-10-26 17:24:21 +0300

    Add files via upload

---
+++ SoftUni-Fundamentals/Arrays/Ex8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            int origialLength = arrayOne.Length;
+            for (int i = 0; i < origialLength-1; i++)
+            {
+                int[] condensed = new int[arrayOne.Length - 1] ;
+                for (int j = 0; j < arrayOne.Length-1; j++)
+                {
+                    condensed[j] = arrayOne[j] + arrayOne[j + 1];
+                }
+                arrayOne = condensed;
+            }
+            Console.WriteLine(arrayOne[0]);
+        }
+    }
+}
