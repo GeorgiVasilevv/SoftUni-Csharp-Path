ff22e2672dd8e6be1726fe118e23e3691aca069a
User <georgi010205@gmail.com>
2023-10-26 17:22:13 +0300

    Add files via upload

---
+++ SoftUni-Fundamentals/Arrays/Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            };
+
+            if (n>0 &&  n<=7)
+            {
+                Console.WriteLine(daysOfWeek[n-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+        }
+    }
+}
