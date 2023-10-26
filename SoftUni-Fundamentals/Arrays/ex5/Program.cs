aa25c7f8cee67e78b00dbd1f733675e95ce7097f
User <georgi010205@gmail.com>
2023-10-26 17:23:11 +0300

    Add files via upload

---
+++ SoftUni-Fundamentals/Arrays/ex5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).
+                ToArray();
+            int Odd = 0;
+            int Even = 0;
+            foreach (var item in nums)
+            {
+                if (item%2==0)
+                {
+                    Even += item;
+                }
+                else
+                {
+                    Odd += item;
+                }
+            }
+            int sum = Even-Odd;
+            Console.WriteLine(sum);
+        }
+    }
+}
