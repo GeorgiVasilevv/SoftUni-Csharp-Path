f7bfe373537931aee0ebdcf49190c8975f9ec3c7
User <georgi010205@gmail.com>
2023-10-26 17:22:53 +0300

    Add files via upload

---
+++ SoftUni-Fundamentals/Arrays/Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
