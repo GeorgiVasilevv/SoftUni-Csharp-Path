7ac942ff0cea782b9c89407194eeab4c92e073f0
User <georgi010205@gmail.com>
2023-10-26 17:23:28 +0300

    Add files via upload

---
+++ SoftUni-Fundamentals/Arrays/Ex7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] arrayTwo = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                if (arrayOne[i]!=arrayTwo[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                sum += arrayOne[i];
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
