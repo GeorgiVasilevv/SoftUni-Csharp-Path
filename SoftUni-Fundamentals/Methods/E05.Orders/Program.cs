e388d55505b9100c0c9b2f9013e4cab5603a031f
Georgi Vasilev <Georgi010205@gmail.com>
2023-10-28 17:55:42 +0300

    Add files via upload

---
+++ SoftUni-Fundamentals/Methods/E05.Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace E05.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            double items = int.Parse(Console.ReadLine());
+            calculateOrder(product, items);
+        }
+        static void calculateOrder(string product, double items)
+        {
+            switch (product)
+            {
+                case "coffee":
+                    Console.WriteLine($"{1.5 * items:f2}");
+                    break;
+                case "water":
+                    Console.WriteLine($"{1 * items:f2}");
+                    break;
+                case "coke":
+                    Console.WriteLine($"{1.4 * items:f2}");
+                    break;
+                case "snacks":
+                    Console.WriteLine($"{2 * items:f2}");
+                    break;
+
+
+
+                default:
+                    break;
+            }
+        }
+    }
+}
