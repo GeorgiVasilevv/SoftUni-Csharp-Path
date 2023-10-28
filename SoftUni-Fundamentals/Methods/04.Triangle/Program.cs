ecbc005ff016ab699bf83040df7dd4f46560a946
Georgi Vasilev <Georgi010205@gmail.com>
2023-10-28 17:55:16 +0300

    Add files via upload

---
+++ SoftUni-Fundamentals/Methods/04.Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04.Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+            for (int i = 1; i < end; i++)
+            {
+                PrintLine(i);
+            }
+            for (int i = end; i > 0; i--)
+            {
+                PrintLine(i);
+            }
+        }
+        static void PrintLine(int end)
+        {
+            for (int i = 1; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
