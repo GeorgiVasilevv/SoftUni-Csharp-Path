d985243a94641903fb3efcb691ffc65034c28853
Georgi Vasilev <Georgi010205@gmail.com>
2023-10-30 00:06:56 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/tutorials/CustomMath/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomMath
+{
+    public class CustomMath
+    {
+        public static int multiply(int a ,int b)
+        {
+            return a;
+            return b;
+        }
+    }
+}
---
+++ SoftUni-Fundamentals/tutorials/tutorials/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorials
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+        public static string fullThrottle(string a, string b)
+        {
+            string c = a + " " + b;
+            Console.WriteLine("The car is going as fast as it can!");
+            return c; 
+        }
+    }
+    }
