caa2007d550d62a9db045424bfe93e66a7ec0fc0
Georgi Vasilev <Georgi010205@gmail.com>
2023-10-31 22:47:29 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/Lists/E05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E05.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            numbers.RemoveAll(x => x < 0);
+            if (numbers.Count==0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            numbers.Reverse();
+            Console.WriteLine(string.Join(" ", numbers));
+            numbers.Select(x => x);
+            
+        }
+    }
+}
