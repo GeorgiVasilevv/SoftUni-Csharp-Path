1c100bcbdc85c57dc58b60e2866c05d29764a06f
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-04 14:41:06 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/ListsExercise/E03.HouseParty/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E03.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmd = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name=cmd[0];
+                if (cmd.Length==3)
+                {
+                    if (list.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                        continue;
+                    }
+                    list.Add(name);
+                }
+                else if (cmd.Length==4)
+                {
+                    if (!list.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                        continue;
+                    }
+                    list.Remove(name);
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
