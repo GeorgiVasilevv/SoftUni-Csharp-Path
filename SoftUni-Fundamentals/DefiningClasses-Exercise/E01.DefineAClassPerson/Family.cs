85f6bc7d29b9d40bbf040d57d615b0c2bccb60c1
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-08 15:42:51 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/DefiningClasses-Exercise/E01.DefineAClassPerson/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+		private List<Person> people;
+
+		public List<Person> People
+		{
+			get { return people; }
+			set { people = value; }
+		}
+
+		public Family()
+		{
+			People = new List<Person>();
+		}
+		public void AddMember (Person member)
+		{
+			People.Add(member);
+		}
+
+		public Person GetOldestMember()
+		{
+			return this.People.OrderByDescending(p => p.Age).First();
+		}
+
+
+    }
+}
---
+++ SoftUni-Fundamentals/DefiningClasses-Exercise/E01.DefineAClassPerson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //string name = Console.ReadLine();
+            //int age = int.Parse(Console.ReadLine());
+            //Person person = new Person(name,age);
+            //Console.WriteLine(person.Name + " " + person.Age);
+
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                string name = personInfo[0];
+                int age= int.Parse(personInfo[1]);
+
+                Person person = new Person(name,age);
+                family.AddMember(person);
+            }
+            //Person oldest = family.GetOldestMember();
+            //Console.WriteLine($"{oldest.Name} {oldest.Age}");
+
+            List<Person> sortedFamily = family.People
+                .FindAll(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            foreach (var member in sortedFamily)
+            {
+                Console.WriteLine($"{member.Name} - {member.Age}");
+            }
+        }
+    }
+}
