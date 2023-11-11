8a46f64a48c68c339c8970fcdb596d6d2bbb91fc
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-11 14:18:39 +0200

    Add files via uplaod

---
+++ SoftUni-Fundamentals/DefiningClasses-Exercise/E09.PokemonTrainer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E09.PokemonTrainer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+            List<Pokemon> pokemons = new List<Pokemon>();
+            string command;
+            while ((command = Console.ReadLine()) != "Tournament")
+            {
+                string[] trainerInfo = command.Split();
+                string trainerName = trainerInfo[0];
+                string pokemonName = trainerInfo[1];
+                string pokemonElement = trainerInfo[2];
+                int pokemonHealth = int.Parse(trainerInfo[3]);
+
+                Trainer trainer = trainers.Find(t => t.Name == trainerName);
+
+                if (trainer == null)
+                {
+                    trainer = new Trainer(trainerName);
+                    trainer.Pokemons.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+                    trainers.Add(trainer);
+                }
+                else
+                {
+                    trainer.Pokemons.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+                }
+
+
+            }
+            while ((command = Console.ReadLine()) != "End")
+            {
+                foreach (var currTrainer in trainers)
+                {
+                    currTrainer.CheckPokemon(command);
+                }
+            }
+
+            foreach (var currTrainer in trainers.OrderByDescending(t => t.NumberOfBadges))
+            {
+                Console.WriteLine(currTrainer);
+            }
+        }
+    }
+}
---
+++ SoftUni-Fundamentals/DefiningClasses-Exercise/E09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E09.PokemonTrainer
+{
+    internal class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            NumberOfBadges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public void CheckPokemon(string element)
+        {
+            if (Pokemons.Any(p => p.Element == element))
+            {
+                NumberOfBadges++;
+            }
+            else
+            {
+                for (int i = 0; i < Pokemons.Count; i++)
+                {
+                    Pokemon pokemon = Pokemons[i];
+
+                    pokemon.Health -= 10;
+
+                    if (pokemon.Health <=0)
+                    {
+                        Pokemons.Remove(pokemon);
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+                return $"{this.Name} {this.NumberOfBadges} {this.Pokemons.Count}";
+        }
+    }
+}
