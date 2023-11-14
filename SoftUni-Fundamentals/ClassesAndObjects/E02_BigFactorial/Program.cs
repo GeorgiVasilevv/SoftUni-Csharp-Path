fda87f7d2fd68189ea54824928ef56f3e8c71322
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-14 22:04:29 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/ClassesAndObjects/E02_BigFactorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace E02_BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger bigInt = 1;
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+            {
+                bigInt *= i;
+            }
+            Console.WriteLine(bigInt);
+        }
+    }
+}
