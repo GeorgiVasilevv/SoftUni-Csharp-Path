3c5ebfb6c8892f593245c39299ce378985edebeb
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-18 17:01:30 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/ClassesAndObjects/P01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<string> words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                int randomNum=random.Next(0,words.Count);
+                string currentWord = words[i];
+                words[i] = words[randomNum];
+                words[randomNum] = currentWord;
+            }
+            Console.WriteLine(string.Join("\n", words));
+
+        }
+    }
+}
