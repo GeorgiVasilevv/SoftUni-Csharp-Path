d615786b4615235ac28da1775dd379b3c2178f12
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-22 21:18:15 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/DictionaryExcercise/E06.Courses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E06.Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+            string cmd = Console.ReadLine();
+            while (cmd!="end")
+            {
+                string[] tokens = cmd.Split(" : ").ToArray();
+                string courseName = tokens[0];
+                string studentName = tokens[1];
+
+                if (courses.ContainsKey(courseName))
+                {
+                    courses[courseName].Add(studentName);
+                }
+                else
+                {
+                    courses.Add(courseName, new List<string>() { studentName });
+                }
+                cmd = Console.ReadLine();
+            }
+            foreach (var item in courses)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count}");
+                foreach (var item2 in item.Value)
+                {
+                    Console.WriteLine($"-- {item2}");
+                }
+            }
+        }
+    }
+}
