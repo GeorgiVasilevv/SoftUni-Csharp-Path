f077c377ef75ef50ae8d2c31f2b13b899c830ff0
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-22 21:17:22 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/DictionaryExcercise/E02.AMinertask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E02.AMinertask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            string cmd = Console.ReadLine();
+            while (cmd != "stop")
+            {
+                string ore = cmd;
+                int count =int.Parse( Console.ReadLine());
+
+                if (dic.ContainsKey(ore))
+                {
+                    dic[ore] += count;
+                }
+                else
+                {
+                    dic.Add(ore, count);
+                }
+                cmd = Console.ReadLine();
+            }
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
