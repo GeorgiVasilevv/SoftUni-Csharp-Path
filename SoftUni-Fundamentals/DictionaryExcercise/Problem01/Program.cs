3f79197636eb885cdcaa676178a5c0d58c6215f3
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-23 19:53:52 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/DictionaryExcercise/Problem01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char,int> dic = new Dictionary<char,int>();
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                    char currChar = str[i];
+                if (currChar !=' ')
+                {
+                    if (dic.ContainsKey(currChar))
+                    {
+                        dic[currChar]++;
+                    }
+                    else
+                    {
+                        dic.Add(currChar, 1);
+                    }
+                }
+            }
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
