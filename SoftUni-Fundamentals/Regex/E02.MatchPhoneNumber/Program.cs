20985beb58de9d4a5ad6e7522016e937d36ad4dd
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-27 20:50:26 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/Regex/E02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace E02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359([\s|\-])2\1(\d{3})\1(\d{4})\b";
+            string text = Console.ReadLine();
+            MatchCollection matches  = Regex.Matches(text, pattern);
+            List<string> matchess = new List<string>();
+            foreach (Match match in matches)
+            {
+                matchess.Add(match.Value);
+            }
+            Console.WriteLine(string.Join(", ",matchess));
+        }
+    }
+}
