514feca81f70a70a77ab67cc82a9fe25ddfe14cc
Georgi Vasilev <Georgi010205@gmail.com>
2023-11-27 20:50:10 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/Regex/E01.MatchFullName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace E01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<first>[A-Z][a-z]{1,}) (?<last>[A-Z][a-z]{1,})\b";
+            string names = Console.ReadLine();
+            MatchCollection validNames = Regex.Matches(names, pattern);
+
+            foreach (Match match in validNames)
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
