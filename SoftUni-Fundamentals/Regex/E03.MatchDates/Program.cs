2eca4418ad9d5302efbb0a0de007728e07dd4c70
Georgi Vasilev <Georgi010205@gmail.com>
2023-12-20 21:09:54 +0200

    Add files via upload

---
+++ SoftUni-Fundamentals/Regex/E03.MatchDates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace E03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})(\.|\-|\/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            string dates = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(dates, pattern);
+            foreach (Match match in matches)
+            {
+                int day = int.Parse(match.Groups["day"].Value);
+                string month = match.Groups["month"].Value;
+                int year = int.Parse(match.Groups["year"].Value);
+                Console.WriteLine($"Day: {day:d2}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
