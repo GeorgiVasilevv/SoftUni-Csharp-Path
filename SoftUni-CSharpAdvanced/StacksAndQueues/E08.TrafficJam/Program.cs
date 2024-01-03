cdb28ace3a5c17b6d40afc3abf2c2a5598c68c20
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-03 18:46:02 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueues/E08.TrafficJam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E08.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int passingCars = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+
+            string command;
+            int passedCarsCount = 0;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                
+
+                if (command == "green")
+                {
+                    for (int i = 0; i < passingCars; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine(queue.Dequeue() + " passed!");
+                        passedCarsCount++;
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{passedCarsCount} cars passed the crossroads.");
+        }
+    }
+}
