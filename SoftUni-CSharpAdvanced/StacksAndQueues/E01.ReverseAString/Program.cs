7d90c6b6d2e36fecab88eea5a9597b442430975b
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-03 18:44:44 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueues/E01.ReverseAString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var @char in input)
+            {
+                stack.Push(@char);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
