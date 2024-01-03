b76f5de690e4dff547bb6c1c5fc58c6665e6b03a
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-03 18:45:34 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueues/E05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Queue<int> indexes = new Queue<int>();
+
+            foreach (var number in input)
+            {
+                if (number %2 ==0 )
+                {
+                    indexes.Enqueue(number);
+                }
+            }
+            
+            Console.Write(String.Join(", ",indexes));
+            
+        }
+    }
+}
