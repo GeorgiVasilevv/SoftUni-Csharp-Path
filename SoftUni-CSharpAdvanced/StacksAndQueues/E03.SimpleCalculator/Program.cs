745c10aa502135e52869a78c8bdd2882f82b0498
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-03 18:45:21 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueues/E03.SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<string> stack = new Stack<string>(Console.ReadLine().Split().Reverse());
+
+            int result = int.Parse(stack.Pop());
+
+            while (stack.Count >= 1)
+            {
+
+
+                if (stack.Pop() == "+" )
+                {
+                    result += int.Parse(stack.Pop());
+
+                }
+                else
+                {
+                    result -= int.Parse(stack.Pop());
+
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
