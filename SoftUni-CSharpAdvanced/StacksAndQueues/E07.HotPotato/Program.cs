ba9132cc1f285b6ebd6fed2958293935e98e14da
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-03 18:45:55 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueues/E07.HotPotato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace E07.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            var kids = new Queue<string>(input);
+            int tosses = 1;
+
+            while (kids.Count > 1)
+            {
+                string currentKid = kids.Dequeue();
+
+                if (tosses==n)
+                {
+                    Console.WriteLine($"Removed {currentKid}");
+                    tosses = 1;
+                    continue;
+                }
+                tosses++;
+                kids.Enqueue(currentKid);
+            }
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
