a6e61ec74b859a66ee69efaa9555fb5916bc2c0e
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-03 18:45:48 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueues/E06.Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace E06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> queue = new Queue<string>();
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split(' ');
+
+                if (tokens[0] == "Paid")
+                {
+                    while (queue.Count != 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(tokens[0]);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(queue.Count + " people remaining.");
+        }
+    }
+}
