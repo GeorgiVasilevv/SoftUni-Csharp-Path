cd0582e5caabc1b970b1b55fe993af683629c047
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-06 16:51:12 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueuesExercise/Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>() 
+            {
+                {"key1", "value1" }
+            };
+
+            Console.WriteLine(dict["key1"]);
+
+        }
+    }
+}
