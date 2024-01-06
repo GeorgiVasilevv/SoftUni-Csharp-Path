e0ce304474626adcc132513427d1b71f76682a56
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-06 16:51:27 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueuesExercise/E02.BasicQueueOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stackArgs = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = stackArgs[0];
+            int numbersToDequeue = stackArgs[1];
+            int numberToLook = stackArgs[2];
+
+            Queue<int> queue = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
+            for (int i = 0; i < numbersToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(numberToLook))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
