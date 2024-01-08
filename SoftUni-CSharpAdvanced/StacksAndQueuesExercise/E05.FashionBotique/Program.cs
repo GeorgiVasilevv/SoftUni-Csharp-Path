8e3242c4e8e33af65785a0fdeaa917ae28f5fde3
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-08 18:37:14 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueuesExercise/E05.FashionBotique/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E05.FashionBotique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int rackCounter = 0;
+            int currentRackCapacity = 0;
+
+            while (clothes.Count > 0)
+            {
+                if (currentRackCapacity - clothes.Peek() < 0)
+                {
+                    rackCounter++;
+                    currentRackCapacity = rackCapacity;
+                }
+                else
+                {
+                    currentRackCapacity -= clothes.Pop();
+                }
+            }
+            Console.WriteLine(rackCounter) ;
+        }
+    }
+}
