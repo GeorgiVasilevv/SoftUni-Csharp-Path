fa30e8f2e1d6b35da8c99f42003464f36e1627b1
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-08 18:36:54 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/StacksAndQueuesExercise/E04.FastFood/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityFood = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            Console.WriteLine(orders.Max());
+            while (orders.Count > 0 )
+            {
+                if (quantityFood>=orders.Peek())
+                {
+                    quantityFood-=orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    return;
+                }
+            }
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
