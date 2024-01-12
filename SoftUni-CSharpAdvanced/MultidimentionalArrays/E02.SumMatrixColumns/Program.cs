2ffc1756cea61c36adf33c92edabc7f1e7b77478
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-12 17:28:53 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/MultidimentionalArrays/E02.SumMatrixColumns/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace E02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrayArgs = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = arrayArgs[0];
+            int columns = arrayArgs[1];
+            int[,] matrix = new int[rows, columns];
+            for (int row = 0; row < rows; row++)
+            {
+                string[] colData = Console.ReadLine().Split(" ");
+
+                for (int col = 0; col < columns; col++)
+                {
+                    matrix[row, col] = int.Parse(colData[col]);
+                }
+            }
+
+            for (int col = 0; col < columns; col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row,col];
+                }
+                Console.WriteLine(sum);
+            }
+            
+        }
+    }
+}
