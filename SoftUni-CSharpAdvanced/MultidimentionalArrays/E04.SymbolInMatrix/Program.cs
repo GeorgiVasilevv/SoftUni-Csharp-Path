f39874751d2d2c116634258ca731d92ef18d16da
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-13 22:08:35 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/MultidimentionalArrays/E04.SymbolInMatrix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace E04.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int r = 0; r < n; r++)
+            {
+                string colData = Console.ReadLine();
+                for (int c = 0; c < n; c++)
+                {
+                    matrix[r, c] = colData[c];
+                }
+            }
+            char charToLook = char.Parse(Console.ReadLine());
+            int AsciiChar = (int)charToLook;
+
+            for (int r = 0; r < n; r++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    if (matrix[r,c] ==AsciiChar)
+                    {
+                        Console.WriteLine($"({r}, {c})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{charToLook} does not occur in the matrix");
+        }
+    }
+}
