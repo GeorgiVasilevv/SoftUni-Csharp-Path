4a3477e7fc93f70f775549b8e8e9298e67f3c84d
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-21 20:05:30 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/MultidimensionalArraysExercise/E06.JaggedArrayManipulator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace E06.JaggedArrayManipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] cols = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                jaggedArray[row] = cols;
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (jaggedArray[row].Length == jaggedArray[row + 1].Length)
+                {
+                    for (int col = 0; col < jaggedArray[row].Length; col++)
+                    {
+                        jaggedArray[row][col] *= 2;
+                        jaggedArray[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jaggedArray[row].Length; col++)
+                    {
+                        jaggedArray[row][col] /= 2;
+                    }
+                    for (int col = 0; col < jaggedArray[row + 1].Length; col++)
+                    {
+                        jaggedArray[row + 1][col] /= 2;
+
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split();
+
+                if (tokens[0] == "Add")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    int value = int.Parse(tokens[3]);
+                    if (IsInputValid(row, col, jaggedArray))
+                    {
+                        jaggedArray[row][col] += value;
+                    }
+                    
+
+                }
+                else if (tokens[0] == "Subtract")
+                {
+                    int row = int.Parse(tokens[1]);
+                    int col = int.Parse(tokens[2]);
+                    int value = int.Parse(tokens[3]);
+                    if (IsInputValid(row, col, jaggedArray))
+                    {
+                        jaggedArray[row][col]-= value;
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    Console.Write(jaggedArray[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool IsInputValid(int row, int col, int[][] jaggedArray)
+        {
+            return row >= 0 && row < jaggedArray.Length
+                && col >= 0 && col < jaggedArray[row].Length;
+        }
+    }
+}
