63b3078db42a61d469323bd96ed1b564187f51fb
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-24 20:23:19 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvanced/E03.Largest3Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E03.Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            nums = nums.OrderByDescending(n => n).ToList();
+
+            int count = nums.Count;
+            if (count > 3)
+            {
+                count = 3;
+            }
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+        }
+    }
+}
