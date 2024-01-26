151f5a7e3f2766e22597c4a468b6b9b2b31cc22c
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-26 23:56:08 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvanced/E06.RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E06.RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+                Console.WriteLine();
+            }
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
