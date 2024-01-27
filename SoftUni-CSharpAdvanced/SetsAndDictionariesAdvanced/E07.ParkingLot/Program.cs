7e9a5a62a833e5200226da9d36d8b2d9cb5eebc0
Georgi Vasilev <Georgi010205@gmail.com>
2024-01-27 11:12:39 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvanced/E07.ParkingLot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] tokens = command.Split(", ");
+                if (tokens[0] == "IN")
+                {
+                    set.Add(tokens[1]);
+                }
+                else if (tokens[0] == "OUT")
+                {
+                    set.Remove(tokens[1]);
+                }
+                command = Console.ReadLine();
+            }
+
+            if (set.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+            foreach (var plateNum in set)
+            {
+                Console.WriteLine(plateNum);
+            }
+        }
+    }
+}
