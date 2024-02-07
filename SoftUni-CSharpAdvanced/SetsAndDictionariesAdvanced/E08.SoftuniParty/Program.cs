dbbe5d3c76ce9be46285790486ec6c25e78bdf57
Georgi Vasilev <Georgi010205@gmail.com>
2024-02-07 22:49:51 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvanced/E08.SoftuniParty/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E08.SoftuniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            string command = Console.ReadLine();
+            while (command != "PARTY")
+            {
+                if (char.IsDigit(command[0]))
+                {
+                    vipGuests.Add(command);
+                }
+                else
+                {
+                    regularGuests.Add(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+            while (command != "END")
+            {
+                vipGuests.Remove(command);
+                regularGuests.Remove(command);
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+            if (vipGuests.Count > 0)
+                Console.WriteLine(String.Join("\n", vipGuests));
+
+            if (regularGuests.Count > 0)
+                Console.WriteLine(String.Join("\n", regularGuests));
+        }
+    }
+}
