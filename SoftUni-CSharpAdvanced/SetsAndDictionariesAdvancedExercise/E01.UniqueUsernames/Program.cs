d29cc9e66c5334c55a127cdfe730baed3a73558e
Georgi Vasilev <Georgi010205@gmail.com>
2024-02-28 22:10:48 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvancedExercise/E01.UniqueUsernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E01.UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+
+            }
+            Console.WriteLine(String.Join("\n", names));
+        }
+    }
+}
