ace90a4bd53a40368693ec1d926fa223b483fe2c
Georgi Vasilev <Georgi010205@gmail.com>
2024-03-11 08:59:51 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvancedExercise/E02.SetsOfElements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E02.SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            HashSet<int> set = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                set.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                set2.Add(int.Parse(Console.ReadLine()));
+            }
+            set.IntersectWith(set2);
+            Console.WriteLine(string.Join(" ", set));
+        }
+    }
+}
