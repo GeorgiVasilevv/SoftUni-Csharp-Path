aed5490866f7e3a68a69ccfe7d4a600544469f60
Georgi Vasilev <Georgi010205@gmail.com>
2024-03-11 09:00:21 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvancedExercise/E04.EvenTimes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,int> keyValuePairs = new Dictionary<int,int>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!keyValuePairs.ContainsKey(number))
+                {
+                    keyValuePairs.Add(number, 0);
+                }
+                keyValuePairs[number]++;
+
+            }
+
+            Console.WriteLine(keyValuePairs.Single(x=> x.Value%2==0).Key);
+        }
+    }
+}
