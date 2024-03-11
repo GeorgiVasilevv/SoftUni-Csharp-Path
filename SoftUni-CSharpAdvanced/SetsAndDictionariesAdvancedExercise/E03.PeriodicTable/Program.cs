62f3e0056da52b73f369442f089a18b6bb716fad
Georgi Vasilev <Georgi010205@gmail.com>
2024-03-11 09:00:08 +0200

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvancedExercise/E03.PeriodicTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E03.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> periodicTable = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                periodicTable.UnionWith(tokens);
+            }
+            Console.WriteLine(string.Join(" ",periodicTable.OrderBy(x=> x)));
+        }
+    }
+}
