62eb6f14be3f3beaef4c01c48554b719cc1fd0dc
Georgi Vasilev <Georgi010205@gmail.com>
2024-05-03 15:45:42 +0300

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvancedExercise/E05.CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E05.CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char,int> chars = new SortedDictionary<char,int>();
+
+            string word = Console.ReadLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+
+                if (!chars.ContainsKey(ch))
+                {
+                    chars.Add(ch, 0);
+                }
+                chars[ch]++;
+            }
+
+            foreach (var @char in chars)
+            {
+                Console.WriteLine($"{@char.Key}: {@char.Value} time/s");
+            }
+        }
+    }
+}
