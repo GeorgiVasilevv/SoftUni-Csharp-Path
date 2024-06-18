e825f230bdfac61f5128436171cec463d7aa3d70
Georgi Vasilev <Georgi010205@gmail.com>
2024-06-18 13:49:11 +0300

    Add files via upload

---
+++ SoftUni-CSharpAdvanced/SetsAndDictionariesAdvancedExercise/E08.Ranking/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace E08.Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contestInfo = new Dictionary<string, string>();
+            SortedDictionary<string, Dictionary<string, int>> contestParticipants = new SortedDictionary<string, Dictionary<string, int>>();
+
+
+            string command = Console.ReadLine();
+            while (command != "end of contests")
+            {
+                string[] tokens = command.Split(":");
+                string contest = tokens[0];
+                string password = tokens[1];
+                contestInfo.Add(contest, password);
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+            while (command != "end of submissions")
+            {
+                string[] tokens = command.Split("=>");
+                string contest = tokens[0];
+                string password = tokens[1];
+                string participantName = tokens[2];
+                int points = int.Parse(tokens[3]);
+
+                if (!contestInfo.ContainsKey(contest) || contestInfo[contest] != password)
+                {
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                if (!contestParticipants.ContainsKey(participantName))
+                {
+                    contestParticipants.Add(participantName, new Dictionary<string, int>());
+                }
+                if (!contestParticipants[participantName].ContainsKey(contest))
+                {
+                    contestParticipants[participantName].Add(contest, 0);
+                }
+                if (contestParticipants[participantName][contest] < points)
+                {
+                    contestParticipants[participantName][contest] = points;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            string bestCandidate = contestParticipants.OrderByDescending(x => x.Value.Values.Sum()).Select(x=>x.Key).First();
+            int maxPoints = contestParticipants[bestCandidate].Values.Sum();
+
+
+            Console.WriteLine($"Best candidate is {bestCandidate} with total {maxPoints} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var candidate in contestParticipants)
+            {
+                Console.WriteLine(candidate.Key);
+                foreach (var info in candidate.Value.OrderByDescending(x=>x.Value))
+                {
+                    Console.WriteLine($"#  {info.Key} -> {info.Value}");
+                }
+            }
+
+        }
+    }
+}
--- SoftUni-Web/CSharpWebFundamentals-TaskBoardApp-WorkShop/TaskBoardApp.Logic/obj/Debug/net6.0/TaskBoardApp.Core.AssemblyInfo.cs
+++ SoftUni-Web/CSharpWebFundamentals-TaskBoardApp-WorkShop/TaskBoardApp.Logic/obj/Debug/net6.0/TaskBoardApp.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskBoardApp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21768a36ddf8aad76a73709d37740ab25d2c57f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskBoardApp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskBoardApp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
